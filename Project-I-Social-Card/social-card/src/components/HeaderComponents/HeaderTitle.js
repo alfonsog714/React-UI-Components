9aa63041f5291706ae6a2e6371391809b2f5b185
Alfonso <alfonso.garcia2324@gmail.com>
2019-04-22 14:50:24 -0700

    Added moment dependability, changed date to moment

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,11 +1,12 @@
 import React from "react";
+import moment from "moment";
 import "./Header.css";
 
 const HeaderTitle = () => {
   return (
     <div className="header-title">
       <h3>Lambda School</h3>
-      <h4>@LambdaSchool - 22 Apr</h4>
+      <h4>@LambdaSchool - {moment().format("ll")}</h4>
     </div>
   );
 };
