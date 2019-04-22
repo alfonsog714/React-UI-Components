e3cf1251fdf6188e00699f643bf84f4de0ba6c3d
Alfonso <alfonso.garcia2324@gmail.com>
2019-04-22 14:25:28 -0700

    Added link attr to anchor tag

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,7 +6,11 @@ import CardBanner from "./CardBanner";
 const CardContainer = () => {
   return (
     <div className="card-container">
-      <a href="https://www.reactjs.org" target="_blank">
+      <a
+        href="https://www.reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <CardBanner />
         <CardContent />
       </a>
