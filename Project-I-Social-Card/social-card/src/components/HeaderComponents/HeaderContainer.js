cffe1cf15646a89e8ec09a67310d074f9a1f585b
Alfonso <alfonso.garcia2324@gmail.com>
2019-04-22 13:28:56 -0700

    Header content and image added

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import ImageThumbnail from "./ImageThumbnail";
+import HeaderContent from "./HeaderContent";
+
+const HeaderContainer = () => {
+  return (
+    <div className="header-box">
+      <ImageThumbnail />
+      <HeaderContent />
+    </div>
+  );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className="header-title">
+      <h3>Lambda School</h3>
+      <h4>@LambdaSchool - 22 Apr</h4>
+    </div>
+  );
+};
+
+export default HeaderTitle;
