b4afbf573ffadfedf6361b54a2b6d61c16fa97dd
Alfonso <alfonso.garcia2324@gmail.com>
2019-04-23 13:33:21 -0700

    Finished calc design, working on functionality

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Button } from "./components/ButtonComponents/NumberButton";
+import { Input } from "./components/DisplayComponents/CalculatorDisplay";
+import { ClearButton } from "./components/ButtonComponents/ActionButton";
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: ""
+    };
+  }
+  addToInput = val => {
+    this.setState({ input: this.state.input + val });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="calc-wrapper">
+          <Input input={this.state.input} />
+          <ClearButton handleClear={() => this.setState({ input: "" })}>
+            Clear
+          </ClearButton>
+          <div className="row">
+            <Button handeClick={this.addToInput}>7</Button>
+            <Button handeClick={this.addToInput}>8</Button>
+            <Button handeClick={this.addToInput}>9</Button>
+            <Button handeClick={this.addToInput}>/</Button>
+          </div>
+          <div className="row">
+            <Button handeClick={this.addToInput}>4</Button>
+            <Button handeClick={this.addToInput}>5</Button>
+            <Button handeClick={this.addToInput}>6</Button>
+            <Button handeClick={this.addToInput}>*</Button>
+          </div>
+          <div className="row">
+            <Button handeClick={this.addToInput}>1</Button>
+            <Button handeClick={this.addToInput}>2</Button>
+            <Button handeClick={this.addToInput}>3</Button>
+            <Button handeClick={this.addToInput}>+</Button>
+          </div>
+          <div className="row">
+            <Button handeClick={this.addToInput}>.</Button>
+            <Button handeClick={this.addToInput}>0</Button>
+            <Button handeClick={this.addToInput}>=</Button>
+            <Button handeClick={this.addToInput}>-</Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
